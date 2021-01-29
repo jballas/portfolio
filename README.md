0c1baa21d5d8f9225a4f60b3aa3c82f4cdb3821e
Julia Ballas <42751526+jballas@users.noreply.github.com>
2021-01-29 10:05:16 -0700

    portfolio update
    I mainly updated the Portfolio color scheme and text. Also updated list of skills. Fixed the jquery so it hides the buttons now. Small edits to css font sizes, line height and added a identity.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 graphic design and web portfolio for Julia Ballas
 
 site: https://jballas.github.io/portfolio
+
+updated 1-29-21
--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
 $(".show_btn").click(function(){
+  $(".show_btn").hide();
   $('.section').slideToggle();
 })
 
