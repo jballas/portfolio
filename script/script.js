1d5a15f310c33d762b70100b4c109a7fe989833c
Julia Ballas <42751526+jballas@users.noreply.github.com>
2021-06-01 14:14:14 -0600

    updated case-study
    Updated case study to have slideToggle animation. Fixed the css to continue to be responsive. Adjusted images. On hover they'll enlarge slightly.

--- script/script.js
+++ script/script.js
@@ -1,8 +1,33 @@
 $(document).ready(function(){
 
-$(".show_btn").click(function(){
-  $(".show_btn").hide();
-  $('.section').slideToggle();
-})
+  $(".show_btn").click(function(){
+    $(".show_btn").hide();
+    $('.section').slideToggle();
+  });
+
+  $("#step1").click(function(){
+      $("#box1").slideToggle("slow", function(){
+        $("#step1").hide();
+      });
+   });
+
+   $("#step2").click(function(){
+       $("#box2").slideToggle("slow", function(){
+         $("#step2").hide();
+       });
+   });
+
+    $("#step3").click(function(){
+        $("#box3").slideToggle("slow", function(){
+          $("#step3").hide();
+        });
+   });
+
+     $("#step4").click(function(){
+         $("#box4").slideToggle("slow", function(){
+           $("#step4").hide();
+         });
+      });
+
 
 });
